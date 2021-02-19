8a901601117431bce5696e3d49521145d3ec57b3
Marcelo Santos <klvo.gtfo@gmail.com>
2021-02-19 16:44:49 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/marcelosantos/cursomc/domain/Produto.java
+++ src/main/java/com/marcelosantos/cursomc/domain/Produto.java
@@ -15,7 +15,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -28,7 +27,7 @@ public class Produto  implements Serializable{
 	private String nome;
 	private Double preço;	
 	
-	@JsonBackReference // DO outro lado da associação ja foram buscados os objetos entao desse lado não sera buscado mais
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name ="PRODUTO_CATEGORIA",
 		joinColumns = @JoinColumn(name = "produto_id"),
